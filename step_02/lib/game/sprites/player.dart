7458fd5f3839eafa931d2d3efc04465d7b0aca05
Mason Williams <bellowingbutcher@gmail.com>
2023-02-13 10:48:04 -0500

    Added sprites and left or right movement

--- step_02/lib/game/sprites/player.dart
+++ step_02/lib/game/sprites/player.dart
@@ -48,24 +48,31 @@ class Player extends SpriteGroupComponent<PlayerState>
 
     // Core gameplay: Add circle hitbox to Dash
 
-    // Add a Player to the game: loadCharacterSprites
-    // Add a Player to the game: Default Dash onLoad to center state
+    await _loadCharacterSprites();
+    current = PlayerState.center;
   }
 
   @override
   void update(double dt) {
     // Add a Player to the game: Add game state check
+    if (gameRef.gameManager.isIntro || gameRef.gameManager.isGameOver) return;
 
     // Add a Player to the game: Add calcualtion for Dash's horizontal velocity
+    _velocity.x = _hAxisInput * jumpSpeed;
 
     final double dashHorizontalCenter = size.x / 2;
 
     // Add a Player to the game: Add infinite side boundaries logic
-
-    // Core gameplay: Add gravity
-
+    if (position.x < dashHorizontalCenter) {
+      position.x = gameRef.size.x - (dashHorizontalCenter);
+    }
     // Add a Player to the game: Calculate Dash's current position based on
     // her velocity over elapsed time since last update cycle
+    if (position.x > gameRef.size.x - (dashHorizontalCenter)) {
+      position.x = dashHorizontalCenter;
+    }
+    // Core gameplay: Add gravity
+    position += _velocity * dt;
     super.update(dt);
   }
 
@@ -80,14 +87,16 @@ class Player extends SpriteGroupComponent<PlayerState>
 
   void moveLeft() {
     _hAxisInput = 0;
-
     // Add a Player to the game: Add logic for moving left
+    current = PlayerState.left;
+    _hAxisInput += movingLeftInput;
   }
 
   void moveRight() {
     _hAxisInput = 0;
-
     // Add a Player to the game: Add logic for moving right
+    current = PlayerState.right;
+    _hAxisInput += movingRightInput;
   }
 
   void resetDirection() {
